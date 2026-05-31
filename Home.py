ccd4fd5df7df6a9bbea998f6b82cb6da4f503234
masonk11-ui <masonakaut@gmail.com>
2026-05-31 12:13:03 -0400

    Updated elim teams - NBA Finals

--- Home.py
+++ Home.py
@@ -64,7 +64,8 @@ st.subheader(f'Team Ratings & Per Game Stats — {now} CT')
 
 eliminated_teams = ['Phoenix Suns', 'Denver Nuggets', 'Houston Rockets', 'Portland Trail Blazers',
                     'Boston Celtics', 'Atlanta Hawks', 'Toronto Raptors', 'Orlando Magic', 'Philadelphia 76ers',
-                    'Detroit Pistons', 'Los Angeles Lakers', 'Minnesota Timberwolves']
+                    'Detroit Pistons', 'Los Angeles Lakers', 'Minnesota Timberwolves', 'Cleveland Cavaliers', 
+                    'Oklahoma City Thunder']
 
 team_metrics["TEAM"] = team_metrics["TEAM"].apply(
     lambda x: f"{x} (e)" if x in eliminated_teams else x
